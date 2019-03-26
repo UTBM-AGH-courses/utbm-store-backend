c04cd0f8c71ec947d64ee43be441143060c45ada
Valentin Reversat <vreversa@utbm.fr>
2019-03-26 12:26:01 +0100

    fix: fk issue

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
+    has_many :orderlines
     has_many :products, through: :orderlines
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    has_many :orderlines
+    has_many :orders, through: :orderlines
 end
